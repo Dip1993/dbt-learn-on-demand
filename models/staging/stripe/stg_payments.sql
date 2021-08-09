c3b73e3a53f3dc13765333cfaf62ffa17eef37c9
dipss.bhattacharyya@gmail.com <dipss.bhattacharyya@gmail.com>
2021-08-09 18:00:16 +0000

    commit after learning about source

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -7,7 +7,7 @@ with payment as (
         status,
         amount/100 as amount, -- as the amount is in cents. So converting to dollars
         created as created_at
-    from raw.stripe.payment
+    from {{ source('stripe', 'payment') }}
 )
 
 select * from payment
